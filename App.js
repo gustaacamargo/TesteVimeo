b4da242012688daa55370be1bee9944e0c8b9e48
Gustavo <camargo25.gustavo@gmail.com>
2021-01-18 15:49:41 -0300

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const [uri, setUri] = useState('')
+
+  useEffect(() => {
+    const VIDEO_ID = '76979871';
+    fetch(`https://player.vimeo.com/video/${VIDEO_ID}/config`)
+      .then(res => res.json())
+      .then(res => {
+        setUri(res.request.files.hls.cdns[res.request.files.hls.default_cdn].url)
+        console.log({
+          thumbnailUrl: res.video.thumbs['640'],
+          videoUrl: res.request.files.hls.cdns[res.request.files.hls.default_cdn].url,
+          video: res.video,
+        });
+      });
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ height: 250, width: '100%' }}>
+        <WebView
+          source={{ uri: uri }}
+        />
+      </View>
+
     </View>
   );
 }
