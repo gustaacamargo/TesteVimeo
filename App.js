b5488070583681437805533e5a67dfc57e7b2850
Gustavo Camargo <camargo25.gustavo@gmail.com>
2021-01-27 14:32:03 -0300

    teste youtube

--- App.js
+++ App.js
@@ -1,32 +1,42 @@
+import { Video } from 'expo-av';
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
+import ytdl from "react-native-ytdl"
 
 export default function App() {
   const [uri, setUri] = useState('')
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const VIDEO_ID = '76979871';
-    fetch(`https://player.vimeo.com/video/${VIDEO_ID}/config`)
-      .then(res => res.json())
-      .then(res => {
-        setUri(res.request.files.hls.cdns[res.request.files.hls.default_cdn].url)
-        console.log({
-          thumbnailUrl: res.video.thumbs['640'],
-          videoUrl: res.request.files.hls.cdns[res.request.files.hls.default_cdn].url,
-          video: res.video,
-        });
-      });
+    async function teste() {
+      const youtubeURL = 'https://www.youtube.com/watch?v=9Co2T3hZ4qI';
+      const urls = await ytdl(youtubeURL);
+      setUri(urls[0].url)
+      console.log(urls);
+      setLoading(false)
+    }
+    teste()
   }, [])
 
   return (
     <View style={styles.container}>
-      <View style={{ height: 250, width: '100%' }}>
-        <WebView
-          source={{ uri: uri }}
+      {loading ? (
+        <Text>Carregando</Text>
+      ) : (
+        <Video
+          source={{ uri }}
+          rate={1.0}
+          volume={1.0}
+          isMuted={false}
+          resizeMode="cover"
+          shouldPlay
+          isLooping
+          useNativeControls
+          style={{ width: 300, height: 300 }}
         />
-      </View>
+      )}
 
     </View>
   );
